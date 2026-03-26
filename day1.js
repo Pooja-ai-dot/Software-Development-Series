52dd017f522489327226edd734682abf59813436
Pooja <poooja.vermaa800@gmail.com>
2026-03-26 20:48:14 +0530

    Implement Day 1 JavaScript Basics
    Added JavaScript basics including variables, data types, and practice examples.

---
+++ day1.js
@@ -0,0 +1,42 @@
+// Day 1 - JavaScript Basics
+
+// 1. Variables
+let name = "Pooja";   // can change
+const age = 20;       // cannot change
+
+// 2. Data Types
+let city = "Delhi";        // String
+let marks = 95;            // Number
+let isStudent = true;      // Boolean
+
+// 3. Output using console.log
+console.log("My name is " + name);
+console.log("My age is " + age);
+console.log("I live in " + city);
+console.log("Marks: " + marks);
+console.log("Am I a student? " + isStudent);
+// Outputs:
+My name is Pooja
+My age is 20
+I live in Delhi 
+Marks: 95
+Am I a student? true
+// 4. Practice Examples
+
+// Example 1: Print simple message
+console.log("Hello, I am learning JavaScript 🚀");
+
+// Example 2: Combine variables
+console.log("My name is " + name + " and I am " + age + " years old");
+// Output: 
+My name is Pooja and I am 20 years old
+// Example 3: Change variable (let can change)
+name = "Pooja Sharma";
+console.log("Updated name: " + name);
+// Output:
+Pooja Sharma
+// Example 4: Boolean check
+let isAdult = true;
+console.log("Is adult: " + isAdult);
+// Output:
+Is adult: true
