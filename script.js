7b15d3f6596969a12cfd3be564edfd33654d755d
Pooja <poooja.vermaa800@gmail.com>
2026-04-12 12:39:01 +0530

    Add CSS styles for card layout and button

---
+++ script.js
@@ -0,0 +1,34 @@
+body{
+    background-color:white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: Arial;
+}
+.card{
+    background-color: white;
+    padding: 20px;
+     border-radius:  10px; 
+     text-align: center;
+     box-shadow: 0 0 10px gray;
+}
+h1{
+    color: #333;
+}
+.info{
+    color:gray;
+}
+button {
+  margin-top: 10px;
+  padding: 10px;
+  border: none;
+  background-color: blue;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: darkblue;
+}
